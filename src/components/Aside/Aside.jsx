57697393715505ca456ecc299e21a895b28018b8
Luizitho36 <luizitho36@hotmail.com>
2024-06-07 14:54:01 -0300

    Improving the CSS Code

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -3,14 +3,16 @@ import { NavLink } from "react-router-dom";
 
 function Aside() {
   return (
-    <p className="pAside">
-      <br></br>
-      {/* Publicidad sencilla para dirigirse al Store */}
-      Todos los juegos de todas las plataformas al mejor precio
-      <NavLink to="/store">
-        <button className="buttonAside">Haz click aquí</button>
-      </NavLink>
-    </p>
+    <div className="divAside">
+      <p className="pAside">
+        <br></br>
+        {/* Publicidad sencilla para dirigirse al Store */}
+        Todos los juegos de todas las plataformas al mejor precio
+        <NavLink to="/store">
+          <button className="buttonAside">Haz click aquí</button>
+        </NavLink>
+      </p>
+    </div>
   );
 }
 
